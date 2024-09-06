2b110711da03d7c53ce67165297fa68be7fe718c
Sibam <sibamboss98@gmail.com>
2024-09-06 21:45:22 +0530

    Updated version

--- MyCircleImageView/build.gradle.kts
+++ MyCircleImageView/build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
     publications {
         register<MavenPublication>("release") {groupId = "com.metafurygames"
             artifactId = "mycircleimageview"
-            version = "1.0.0"
+            version = "1.1.0"
 
             afterEvaluate {
                 from(components["release"])
@@ -57,7 +57,7 @@ publishing {
 
     repositories {
         maven {
-            url = uri("https://maven.pkg.github.com/sibam99/CIrcleImage") // Replace with your repository URL
+            url = uri("https://github.com/sibam99/CIrcleImage") // Replace with your repository URL
             credentials {
                 username = System.getenv("GITHUB_USERNAME")
                 password = System.getenv("GITHUB_TOKEN")
